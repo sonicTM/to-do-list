5f01130661bea9f080777c2c0d2ed8c5cb18abc2
Trudi Machin <ati.machin3@gmail.com>
2021-04-21 19:30:07 +0100

    interface

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root', // name of tag we will use in component
@@ -10,7 +11,7 @@ export class AppComponent {
   title = 'todo-list';
 
   // array of todos
-  todoList = [
+  todoList: TodoItem[] = [
     {title: 'install NodeJS'},
     {title: 'install Angular CLI'},
     {title: 'create new app'},
